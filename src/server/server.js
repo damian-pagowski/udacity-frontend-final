778adf647792a8a31940b756eaebacefe0659644
Damian Pagowski <d.pagowski@gmail.com>
2020-04-09 17:39:01 +0700

    added images API support

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const express = require("express");
+const moment = require("moment");
+
 const app = express();
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 dotenv.config();
 
 app.use(express.static("dist"));
@@ -13,43 +16,52 @@ app.use(
   })
 );
 
-// set aylien API credentias
-const AYLIENTextAPI = require("aylien_textapi");
-const textapi = new AYLIENTextAPI({
-  application_id: process.env.API_ID,
-  application_key: process.env.API_KEY,
-});
-
 console.log(__dirname);
 
-app.get("/", (req, res) => {
+const pixabayAPI = process.env.API_URL_PIXABAY;
+const pixabayKey = process.env.API_KEY_PIXABAY;
+
+function getImagesData(destination) {
+  const url = `${pixabayAPI}?key=${pixabayKey}&q=${destination}&image_type=photo&page=1&per_page=3`;
+  return fetch(url).then((res) => res.json());
+}
+const extractImage = (imageJson) => imageJson.hits[0].webformatURL;
+
+app.get("/", async (req, res) => {
   res.sendFile("dist/index.html");
 });
 
+app.post("/trip", async (req, res) => {
+  const { date, destination } = req.body;
+  const day = moment(date, "DD/MM/YYYY").toDate();
 
-app.post("/test", async (req, res) => {
-  const { url } = req.body;
-  const isValid = /^(ftp|http|https):\/\/[^ "]+$/.test(url)
-  if (!isValid){
-    return res.json({error: "Invalid URL!"})
-  }
-  const params = {
-    url,
-  };
-  const cb = (error, response) => {
-    if (error === null) {
-      console.log(response);
-      res.send({
-        url,
-        language: response.language,
-        categories: response.categories,
-      });
-    }
-  };
-  textapi.classify(params, cb);
+  const imageJson = await getImagesData(destination);
+
+
+  res.json({ image: extractImage(imageJson) });
+
+  // GET FETCH EXAMPLE
+  // fetch("https://api.github.com/users/github")
+  //   .then((res) => res.json())
+  //   .then((json) => console.log(json));
+
+  // POST
+
+  // const body = { a: 1 };
+
+  // fetch("https://httpbin.org/post", {
+  //   method: "post",
+  //   body: JSON.stringify(body),
+  //   headers: { "Content-Type": "application/json" },
+  // })
+  //   .then((res) => res.json())
+  //   .then((json) => console.log(json));
+
+  // //
 });
 
+const PORT = 3030;
 // designates what port the app will listen to for incoming requests
-app.listen(8080, () => {
-  console.log("Example app listening on port 8080!");
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
